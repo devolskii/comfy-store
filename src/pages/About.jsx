7b8075982ed719423985613550d1204247d7c02f
devolskii <devolskii@gmail.com>
2025-04-14 20:38:48 +0530

    Add About Page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,21 @@
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <div className="flex justify-center gap-6">
+        <h1 className="text-6xl tracking-tight">We love</h1>
+        <div className="stats bg-primary flex items-center">
+          <div className="stat-title text-4xl text-primary-content font-bold tracking-widest px-4">
+            comfy
+          </div>
+        </div>
+      </div>
+      <p className="mt-6 text-lg leading-8 max-w-2xl mx-auto">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus
+        consectetur quasi deleniti quas sed laborum debitis aut, velit inventore
+        voluptates dolor quo ipsum aliquid saepe, eos, suscipit quidem alias
+        non?
+      </p>
+    </>
+  );
 };
 export default About;
