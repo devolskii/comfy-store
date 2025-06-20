4d23df7db4acf5eb9bc70d4bd29270360681c290
devolskii <devolskii@gmail.com>
2025-06-20 18:19:01 +0530

    Checkout Done

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,4 +1,27 @@
+import { useSelector } from "react-redux";
+import { CartTotals, CheckoutForm, SectionTitle } from "../components";
+import { redirect } from "react-router";
+import { toast } from "react-toastify";
+
+export const loader = (store) => async () => {
+  const user = store.getState().userState.user;
+  if (!user) {
+    toast.warn("You must be logged in to checkout");
+    return redirect("/login");
+  }
+};
+
 const Checkout = () => {
-  return <div>Checkout</div>;
+  const cartTotal = useSelector((state) => state.cartState.cartTotal);
+  if (!cartTotal) return <SectionTitle text="your cart is empty!" />;
+  return (
+    <>
+      <SectionTitle text="place your order!" />
+      <div className="lg:grid lg:grid-cols-2 lg:gap-x-6">
+        <CheckoutForm />
+        <CartTotals />
+      </div>
+    </>
+  );
 };
 export default Checkout;
