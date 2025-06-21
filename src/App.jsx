d37cccffc2ee749aa0053b5e0f8aa5b55e68d8ea
devolskii <devolskii@gmail.com>
2025-06-21 15:06:18 +0530

    Remove React Query Devtools

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import {
   About,
@@ -91,7 +90,6 @@ const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 };
